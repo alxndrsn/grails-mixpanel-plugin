222ce2446466dfb2544ca707bc8e336599e7934a
Burt Beckwith <burt@burtbeckwith.com>
2013-01-23 11:20:21 -0500

    added missing dependencies

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -8,11 +8,19 @@ grails.project.dependency.resolution = {
 
 	repositories {
 		grailsCentral()
+		mavenLocal()
+		mavenCentral()
+	}
+
+	dependencies {
+		compile 'com.mixpanel:mixpanel-java:1.1.0'
 	}
 
 	plugins {
 		build(':release:2.2.0', ':rest-client-builder:1.0.3') {
 			export = false
 		}
+
+		compile ':platform-core:1.0.RC5'
 	}
 }
